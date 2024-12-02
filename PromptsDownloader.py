373cfc52c34a717c50d846f4ccf04ea39cafc6a2
hazizb <hazizb@slurm-login-01.auth.ad.bgu.ac.il>
2024-12-02 14:17:06 +0200

    Resolved conflicts

--- PromptsDownloader.py
+++ PromptsDownloader.py
@@ -3,6 +3,8 @@ n_examples = 10
 df = pd.read_parquet("hf://datasets/walledai/AdvBench/data/train-00000-of-00001.parquet")
 df = df.sample(frac=1).iloc[:n_examples]
 harmful = df['prompt']
-print(harmful)
+#print(harmful)
 
-
+df = pd.read_json("hf://datasets/yahma/alpaca-cleaned/alpaca_data_cleaned.json")
+harmless = df[df['input'] == ''].sample(frac=1).iloc[:n_examples]['instruction']
+print(harmless)
